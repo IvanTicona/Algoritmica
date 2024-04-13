5b3eb60a5820612fdd6da3b2dd9f42e3dfcbddff
IvanTicona <ivan.ticona.v@gmail.com>
2024-04-13 15:28:20 -0400

    Accepted C

--- problemas/vjudge/C.cpp
+++ problemas/vjudge/C.cpp
@@ -2,15 +2,45 @@
 
 using namespace std;
 
+vector<int> days(0);
+vector<int> savedDays(0);
+
+void coinsDays() {
+  for (int i = 1; i <= 141; ++i) {
+    for (int j = 0; j < i; ++j) {
+      days.push_back(i);
+    }
+  }
+}
+
+int getTotalCoins(int n) {
+  int s=0;
+  for (int i = 0; i < n; ++i) {
+    s += days[i];
+  }
+  return s;
+}
+
 int main(){
 
-  int r=0;
-  int c=1;
-  for (int i = 1; i <= 6; i+=c){
-    r += c;
-    c++;
+  int nblocks,line,limit;
+  coinsDays();
+  cin>>nblocks;
+  while(nblocks--){
+    limit=21;
+    while(limit-- && cin>>line && line!=0){
+      savedDays.push_back(line);   
+    }
+    savedDays.push_back(0);
   }
-  cout<<r<<endl;
-  
+  savedDays.pop_back();
+  for (int i = 0; i < savedDays.size(); ++i) {
+    if(savedDays[i] == 0){
+      cout<<endl;
+    } else{
+      cout<<savedDays[i]<<" "<<getTotalCoins(savedDays[i])<<endl;
+    }
+  }
+
   return 0;
 }
